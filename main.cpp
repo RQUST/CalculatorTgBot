681e689993c8a08c75b0fffd601271e5f4405e01
RQUST <rqust@yandex.com>
2024-06-14 23:37:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,37 @@
-#include <tgbot/Bot.h>
-
-void Start(TgBot::Message::Ptr message_ptr) {
-  const auto chat_id = message_ptr->chat->id;
-}
+#include <tgbot/tgbot.h>
 
 int main() {
   TgBot::Bot bot("7411232470:AAHTDVw80FPIfhqyMSy34nc0_mcLc9meuQA");
+  auto& api = bot.getApi();
 
-  bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message_ptr) {
+  bot.getEvents().onCommand("start", [&api](TgBot::Message::Ptr message_ptr) {
     const auto chat_id = message_ptr->chat->id;
-    bot.getApi().sendMessage(chat_id, "Bot started");
+    api.sendMessage(chat_id, "Bot started");
   });
 
-  bot.getEvents().onCommand("help", [&bot](TgBot::Message::Ptr message_ptr) {
+  bot.getEvents().onCommand("help", [&api](TgBot::Message::Ptr message_ptr) {
     const auto chat_id = message_ptr->chat->id;
-    bot.getApi().sendMessage(chat_id, "Help message");
+    api.sendMessage(chat_id, "Help message");
   });
 
- bot.getEvents().onCommand("hello", [&bot](TgBot::Message::Ptr message_ptr) {
+  bot.getEvents().onCommand("hello", [&api](TgBot::Message::Ptr message_ptr) {
     const auto chat_id = message_ptr->chat->id;
-    bot.getApi().sendMessage(chat_id, "Hello, " + message_ptr->chat->username);
+    api.sendMessage(chat_id, "Hello, " + message_ptr->chat->username);
   });
+
+  bot.getEvents().onUnknownCommand([&api](TgBot::Message::Ptr message_ptr) {
+    const auto chat_id = message_ptr->chat->id;
+    api.sendMessage(chat_id, "Unknown Command, " + message_ptr->text);
+  });
+
+  api.deleteWebhook();
+  auto long_poll = TgBot::TgLongPoll(bot);
+  while (true) {
+    try {
+      long_poll.start();
+    } catch (const TgBot::TgException& err) {
+      std::cerr << "TgBot error" << err.what() << '\n';
+      continue;
+    }
+  }
 }
